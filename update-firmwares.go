47c93acf4974ec0cbcc30f3ed87a6b6e8e8332ce
Nick Guenther <nick@kousu.ca>
2020-02-27 03:08:39 -0500

    Factor helper routines

--- update-firmwares.go
+++ update-firmwares.go
@@ -37,9 +37,113 @@ type ApiError struct {
 }
 
 var verboseLog *log.Logger
+var (
+	api, hostname, authToken string
+)
 
 // TODO: for unit testing main(), you can use
 // os.Args = []string{"something", "something"}
+
+// Update player clientId with new parameters updateJSON
+func update(clientId string, updateJSON []byte) error {
+	url := api + "/profiles/clientId:" + clientId
+
+	req, err := http.NewRequest(http.MethodPut, url, bytes.NewReader(updateJSON))
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("X-Client-ID", hostname) // Assumption: this is meant to identify the *user*; if it was meant to identify the software it would be "User-Agent"
+	req.Header.Add("X-Authentication-Token", authToken)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode == http.StatusOK {
+		return nil
+	} else {
+		if resp.Header.Get("Content-Type") == "application/json" {
+			var respDetails ApiError
+			err := json.NewDecoder(resp.Body).Decode(&respDetails)
+			if err != nil {
+				return fmt.Errorf("%s %s: %v", req.Method, url, err.Error())
+			} else {
+				return fmt.Errorf("%s %s: %v", req.Method, url, respDetails.Message)
+			}
+		} else {
+			// Nonsense result; maybe we're not talking to the right API?
+			return fmt.Errorf("%s %s: %v", req.Method, url, resp.Status)
+		}
+	}
+}
+
+func batchUpdate(deviceList io.Reader, apps map[string]string) {
+	// Pre-compute the update-request JSON
+	var appSpec []ApplicationUpdateSpec
+	for app, version := range apps {
+		appSpec = append(appSpec, ApplicationUpdateSpec{app, version})
+	}
+
+	updateJSON, err := json.MarshalIndent(
+		ApplicationUpdate{
+			Profile: ApplicationUpdateProfile{
+				Applications: appSpec,
+			},
+		},
+		"",
+		"  ")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	verboseLog.Printf("Updating players with:\n%s\n", string(updateJSON))
+
+	// TODO: add a worker pool here so that updates can happen in parallel.
+
+	devices := csv.NewReader(deviceList)
+
+	header, err := devices.Read()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	headerM := make(map[string]int)
+	for i, h := range header {
+		headerM[h] = i
+	} // go doesn't have an indexOf() method; this is the second-best way
+	macAddresses_i, exists := headerM["mac_addresses"]
+	if !exists {
+		log.Fatalf("Batch input missing 'mac_addresses' column")
+	}
+	r := 0
+	for {
+		r += 1
+		record, err := devices.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+
+		clientId := record[macAddresses_i]
+
+		macAddresses := regexp.MustCompile(`^([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2})$`)
+		if !macAddresses.MatchString(clientId) {
+			log.Printf("Warning: invalid MAC address '%s' on line %d.\n", clientId, r)
+			continue // ignore, don't break, the batch job on invalid lines
+		}
+
+		verboseLog.Printf("Updating '%s'\n", clientId)
+		err = update(clientId, updateJSON)
+		if err != nil {
+			log.Println(err.Error())
+			continue
+		}
+		log.Printf("%s updated.\n", clientId)
+	}
+
+	verboseLog.Println("TouchTunes Fleet Updater Finished")
+}
 
 func main() {
 	log.SetFlags(0) // disable timestamps ; XXX don't do this?
@@ -101,110 +205,23 @@ func main() {
 	verboseLog.Println("TouchTunes Fleet Updater Starting Up")
 
 	// Load environment parameters
-	api, apiSet := os.LookupEnv("TOUCHTUNES_FLEET_API")
-	if !apiSet {
+	var set bool
+	var err error
+	api, set = os.LookupEnv("TOUCHTUNES_FLEET_API")
+	if !set {
 		api = "http://fleet.intra.touchtones.example.com:6565"
 	}
 
-	hostname, err := os.Hostname()
+	hostname, err = os.Hostname()
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 
-	authToken, authTokenSet := os.LookupEnv("TOUCHTUNES_AUTH_TOKEN")
-	if !authTokenSet {
+	authToken, set = os.LookupEnv("TOUCHTUNES_AUTH_TOKEN")
+	if !set {
 		log.Println("Warning: TOUCHTUNES_AUTH_TOKEN should be defined.")
 		authToken = "<required>"
 	}
 
-	// Pre-compute the update-request JSON
-	var appSpec []ApplicationUpdateSpec
-	for app, version := range apps {
-		appSpec = append(appSpec, ApplicationUpdateSpec{app, version})
-	}
-
-	update, err := json.MarshalIndent(
-		ApplicationUpdate{
-			Profile: ApplicationUpdateProfile{
-				Applications: appSpec,
-			},
-		},
-		"",
-		"  ")
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-	verboseLog.Printf("Updating players with:\n%s\n", string(update))
-
-	// TODO: add a worker pool here so that updates can happen in parallel.
-
-	devices := csv.NewReader(input)
-
-	header, err := devices.Read()
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-	headerM := make(map[string]int)
-	for i, h := range header {
-		headerM[h] = i
-	} // go doesn't have an indexOf() method; this is the second-best way
-	macAddresses_i, exists := headerM["mac_addresses"]
-	if !exists {
-		log.Fatalf("Batch input missing 'mac_addresses' column")
-	}
-	r := 0
-	for {
-		r += 1
-		record, err := devices.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalf(err.Error())
-		}
-
-		clientId := record[macAddresses_i]
-
-		macAddresses := regexp.MustCompile(`^([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2})$`)
-		if !macAddresses.MatchString(clientId) {
-			log.Printf("Warning: invalid MAC address '%s' on line %d.\n", clientId, r)
-			continue // ignore, don't break, the batch job on invalid lines
-		}
-
-		url := api + "/profiles/clientId:" + clientId
-		verboseLog.Printf("Updating '%s'\n", url)
-
-		req, err := http.NewRequest(http.MethodPut, url, bytes.NewReader(update))
-		if err != nil {
-			log.Println(err.Error())
-			continue
-		}
-		req.Header.Add("Content-Type", "application/json")
-		req.Header.Add("X-Client-ID", hostname) // Assumption: this is meant to identify the *user*; if it was meant to identify the software it would be "User-Agent"
-		req.Header.Add("X-Authentication-Token", authToken)
-
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			log.Println(err.Error())
-			continue
-		}
-		if resp.StatusCode == http.StatusOK {
-			log.Printf("%s updated.\n", url)
-		} else {
-			if resp.Header.Get("Content-Type") == "application/json" {
-				var respDetails ApiError
-				err := json.NewDecoder(resp.Body).Decode(&respDetails)
-				if err != nil {
-					log.Printf("%s %s: %v\n", req.Method, url, err.Error())
-				} else {
-					log.Printf("%s %s: %v\n", req.Method, url, respDetails.Message)
-				}
-			} else {
-				// Nonsense result; maybe we're not talking to the right API?
-				log.Printf("%s %s: %v\n", req.Method, url, resp.Status)
-			}
-		}
-	}
-
-	verboseLog.Println("TouchTunes Fleet Updater Finished")
+	batchUpdate(input, apps)
 }
